ebec9607afaca49f1c8159f8153f036533c3ca40
hemantgoyal62 <hemant21701@gmail.com>
2022-09-11 17:31:23 +0530

    redo changes

--- website/plugins/react-native-elements-web.js
+++ website/plugins/react-native-elements-web.js
@@ -4,8 +4,6 @@ const webpack = require('webpack');
 const rootNodeModules = path.join(__dirname, '..', '..', 'node_modules');
 const nodeModules = path.join(__dirname, '..', 'node_modules');
 
-// hemant commit 1
-
 /** @type {()=>import('@docusaurus/types').Plugin} */
 const a = function () {
   return {
